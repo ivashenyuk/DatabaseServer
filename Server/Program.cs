ef4996dea43065c3e5311f2cf87078ee480d6b0c
Yuriy Ivashenyuk <yura.ivash@gmail.com>
2018-02-25 22:23:25 +0200

    Start up project

---
+++ Server/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+using System.Threading;
+
+namespace Server
+{
+    [ServiceContract]
+    interface IContract
+    {
+        [OperationContract(Name = "ClickBtn")]
+        void Click();
+        [OperationContract(Name = "ClickPhoto")]
+        int Click(int k);
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
